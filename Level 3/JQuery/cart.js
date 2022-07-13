e0b0494f038df4e319f4ee82442df58c466f9d0f
akmzx <akmzx3801@gmail.com>
2022-07-13 19:03:58 +0630

    day8-assignment

--- Level 3/JQuery/cart.js
+++ Level 3/JQuery/cart.js
@@ -17,49 +17,117 @@ $(function () {
     let cartStr = localStorage.getItem("cart");
     if (!cartStr) {
       var itemArr = new Array();
-      itemArr.push(item);
     } else {
       itemArr = JSON.parse(cartStr);
-      for (let i = 0; i < itemArr.length; i++) {
-        if (itemArr[i].name === name) {
-          itemArr[i].qty += 1;
-          var done = true;
-          break;
-        }
-      }
-      if (!done) itemArr.push(item);
     }
+    var status = false;
+    $.each(itemArr, function (i, v) {
+      if (v.id == id) {
+        v.qty++;
+        status = true;
+        return false;
+      }
+    });
 
+    if (status == false) {
+      itemArr.push(item);
+    }
     localStorage.setItem("cart", JSON.stringify(itemArr));
     getData();
   });
 
   function getData() {
     let cartStr = localStorage.getItem("cart");
-    var cartArr = JSON.parse(cartStr);
-
+    var total = 0;
     var data = "";
+
     if (!cartStr) {
-      data = `<p class="empty">Your Cart is Empty!</p>`;
-      $("#cart").html(data);
+      data = `<h2 class="empty">Your Cart is Empty!</h2>`;
+      $(".table").hide();
+      $(".cart_empty").show();
+      $(".cart_empty").html(data);
     } else {
-      var items = cartArr.map(function (item) {
-        return `<tr>
-          <td>${item.name}</td>
-          <td>${item.price}</td>
-          <td>${item.qty}</td>
-          <td>${item.price * item.qty}</td>
-        </tr>`;
+      var body;
+      var cartArr = JSON.parse(cartStr);
+      var total = 0;
+      $.each(cartArr, function (i, item) {
+        let id = i + 1;
+        total += item.qty * item.price;
+        body += `<tr>
+        <td class="button_td">
+          <p>${id}</p>
+          <button class="deletebtn">X</button>
+        </td>
+        <td>${item.name}</td>
+        <td>${numberFormat(item.price)}</td>
+        <td class="button_td">
+          <button class="minbtn" data-index="${i}">-</button>
+          <p>${item.qty}</p>
+          <button class="maxbtn" data-index="${i}">+</button>
+        </td>
+        <td>${numberFormat(item.price * item.qty)}</td>
+      </tr>`;
       });
-      data += items;
-      $("tbody.product_data").html(data);
-      var prices = cartArr.map(function (item) {
-        return item.price * item.qty;
-      });
-      data = prices.reduce((sum, current) => {
-        return (sum += current);
-      }, 0);
+      body += `<tr>
+      <td colspan="4">Total</td>
+      <td>${numberFormat(total)}</td>
+    </tr>`;
+      $(".cart_empty").hide();
+      $(".product_data").html(body);
+      $(".table").show();
     }
-    $(".total_cost").html(data);
+  }
+
+  // Decrease Qty
+  $(".product_data").on("click", ".minbtn", function () {
+    var index = $(this).data("index");
+    let cartStr = localStorage.getItem("cart");
+    var cartArr = JSON.parse(cartStr);
+    if (cartArr[index].qty > 1) {
+      cartArr[index].qty--;
+    } else {
+      var status = confirm("Are you sure to Delete?");
+      if (status == true) {
+        // delete that row
+        cartArr.splice(index, 1);
+      }
+    }
+    // console.log(cartArr[index]);
+    localStorage.setItem("cart", JSON.stringify(cartArr));
+    if (cartArr.length < 1) {
+      localStorage.clear();
+    }
+    getData();
+  });
+
+  // increase Qty
+  $(".product_data").on("click", ".maxbtn", function () {
+    var index = $(this).data("index");
+    let cartStr = localStorage.getItem("cart");
+    var cartArr = JSON.parse(cartStr);
+    cartArr[index].qty++;
+    localStorage.setItem("cart", JSON.stringify(cartArr));
+    getData();
+  });
+
+  // clear product
+  $(".product_data").on("click", ".deletebtn", function () {
+    var index = $(this).data("index");
+    let cartStr = localStorage.getItem("cart");
+    var cartArr = JSON.parse(cartStr);
+    var status = confirm("Are you sure to Delete?");
+    if (status == true) {
+      // delete that row
+      cartArr.splice(index, 1);
+    }
+    localStorage.setItem("cart", JSON.stringify(cartArr));
+    if (cartArr.length < 1) {
+      localStorage.clear();
+    }
+    getData();
+  });
+
+  function numberFormat(x) {
+    return String(x).replace(/(.)(?=(\d{3})+$)/g, "$1,");
   }
 });
---
+++ Level 3/JQuery/test.js
@@ -0,0 +1,80 @@
+$(function () {
+  getData();
+
+  $("button.atcbtn").on("click", function () {
+    var id = $(this).data("id");
+    var name = $(this).data("name");
+    var price = $(this).data("price");
+
+    var item = {
+      id: id,
+      name: name,
+      price: price,
+      qty: 1,
+    };
+    // console.log(item)
+
+    var cartStr = localStorage.getItem("cart");
+    if (!cartStr) {
+      var itemArr = new Array(); // first time
+    } else {
+      var itemArr = JSON.parse(cartStr);
+    }
+
+    var status = false;
+    $.each(itemArr, function (i, v) {
+      if (v.id == id) {
+        v.qty++;
+        status = true;
+        return false;
+      }
+    });
+
+    if (status == false) {
+      itemArr.push(item);
+    }
+
+    localStorage.setItem("cart", JSON.stringify(itemArr));
+    getData();
+  });
+
+  function getData() {
+    var cartStr = localStorage.getItem("cart");
+    if (!cartStr) {
+      var data = `<h2 style="text-align: center;">Your Cart is Empty!</h2>`;
+      $(".mytable").hide();
+      $(".mycart").show();
+      $(".mycart").html(data);
+    } else {
+      var body;
+      var cartArr = JSON.parse(cartStr);
+      var total = 0;
+      $.each(cartArr, function (i, v) {
+        total += v.qty * v.price;
+        let id = ++i;
+        body += `<tr>
+                        <td>
+                            ${id}
+                            <button class="delebtn">X</button>
+                        </td>
+                        <td>${v.name}</td>
+                        <td>${v.price}</td>
+                        <td>
+                        <button class="minbtn">-</button>
+                        ${v.qty}
+                        <button class="maxbtn">+</button>
+                        </td>
+                        <td>${v.qty * v.price}</td>
+                        </tr>`;
+      });
+      body += `<tr>
+                    <td colspan="4">Total</td>
+                    <td>${total}</td>
+                </tr>`;
+
+      $(".mycart").hide();
+      $("#cartitems").html(body);
+      $(".mytable").show();
+    }
+  }
+});
