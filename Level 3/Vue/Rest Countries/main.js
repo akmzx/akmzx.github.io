f8ce19acab56779ba29486303d583edeb5a41a30
akmzx <akmzx3801@gmail.com>
2022-08-09 15:58:29 +0630

    Vue

---
+++ Level 3/Vue/Rest Countries/main.js
@@ -0,0 +1,114 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      lightMode: true,
+      clicked: false,
+      search: "",
+      selected: "All",
+      options: ["All", "Africa", "Americas", "Asia", "Europe", "Oceania"],
+      // countries: {
+      //   name: Array(),
+      //   native: Array(),
+      //   population: Array(),
+      //   region: Array(),
+      //   subregion: Array(),
+      //   capital: Array(),
+      //   tld: Array(),
+      //   img: Array(),
+      // },
+      countries: Array(),
+      data: Array(),
+      number: Number(),
+      currencyName: Array(),
+      languageName: Array(),
+    };
+  },
+  mounted() {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        // handle success
+        console.log(response.data);
+        this.data = response.data;
+        this.countries = response.data;
+        // let data = response.data;
+        // for (let i = 0; i < data.length; i++) {
+        //   this.countries.name.push(data[i].name.official);
+        //   this.countries.native.push(data[i].name.common);
+        //   this.countries.population.push(data[i].population);
+        //   this.countries.region.push(data[i].region);
+        //   this.countries.subregion.push(data[i].subregion);
+        //   this.countries.capital.push(data[i].capital);
+        //   this.countries.tld.push(data[i].tld);
+        //   this.countries.img.push(data[i].flags.png);
+        // }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+  watch: {
+    selected() {
+      if (this.selected === "All") {
+        this.countries = this.data;
+      } else {
+        this.countries = this.data.filter((country) => {
+          return country.region.includes(this.selected);
+        });
+      }
+    },
+    search() {
+      let result = this.search.toLowerCase();
+      this.selected = "All";
+      if (this.search != "") {
+        this.countries = this.data.filter((country) => {
+          return (
+            country.name.official.toLowerCase().includes(result) ||
+            country.name.common.toLowerCase().includes(result)
+          );
+        });
+      } else {
+        this.countries = this.data;
+      }
+    },
+  },
+
+  methods: {
+    changeTheme() {
+      this.lightMode = !this.lightMode;
+      if (this.lightMode == true) {
+        document.body.classList.remove("dark_mode");
+      } else {
+        document.body.classList.add("dark_mode");
+      }
+    },
+    btnClick() {
+      this.clicked = !this.clicked;
+    },
+    showData(num) {
+      this.number = num;
+      this.btnClick();
+      let currencyObject = this.data[num].currencies;
+      if (currencyObject != undefined) {
+        let currencyValues = Object.values(currencyObject);
+        for (let i = 0; i < currencyValues.length; i++) {
+          this.currencyName.push(currencyValues[i].name);
+        }
+      } else {
+        this.currencyName = "";
+      }
+
+      let languageObject = this.data[num].languages;
+      if (languageObject != undefined) {
+        let languageValues = Object.values(languageObject);
+        var languageName = [];
+        for (let i = 0; i < languageValues.length; i++) {
+          this.languageName.push(languageValues[i]);
+        }
+      }
+    },
+    numberFormat(x) {
+      return String(x).replace(/(.)(?=(\d{3})+$)/g, "$1,");
+    },
+  },
+});
--- Level 3/Vue/main.js
+++ Level 3/Vue/main.js
@@ -22,3 +22,90 @@ const app = Vue.createApp({
     },
   },
 });
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      age: Number,
+      isButtonDisabled: true,
+      isInputDisabled: true,
+      mark: 50,
+      isActive: true,
+      hasError: false,
+      students: [
+        { name: "Mg Mg", age: 22, gender: "male" },
+        { name: "Su Su", age: 23, gender: "female" },
+        { name: "Bo Bo", age: 20, gender: "male" },
+      ],
+    };
+  },
+  methods: {
+    mybtn() {
+      if (this.age >= 18) {
+        this.isInputDisabled = false;
+      }
+    },
+    validate() {
+      console.log("Form validation here!");
+    },
+    alertDetails(index) {
+      alert(
+        `My name is ${this.students[index].name} and I'm ${this.students[index].age} years old`
+      );
+    },
+  },
+});
+
+const app3 = Vue.createApp({
+  data() {
+    return {
+      status: true,
+      name: "Aung Myat",
+      items: Array(),
+    };
+  },
+  mounted() {
+    // this.name = this.name.split("").reverse().join("");
+    // console.log(`the component is now mounted`)
+
+    axios
+      .get("https://api.imgflip.com/get_memes")
+      .then((response) => {
+        // handle success
+        console.log(response.data);
+        if (response.data.success == true) {
+          this.items = response.data.data.memes;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+  methods: {
+    getData() {},
+  },
+});
+
+const app4 = Vue.createApp({
+  data() {
+    return {
+      students: [
+        { name: "Mg Mg", age: 16, gender: "male" },
+        { name: "Su Su", age: 23, gender: "female" },
+        { name: "Bo Bo", age: 20, gender: "male" },
+      ],
+    };
+  },
+  methods: {},
+  computed: {
+    boys() {
+      return this.students.filter((row) => row.gender == "male");
+    },
+    adults() {
+      return this.students.filter((row) => row.age >= 18);
+    },
+    kyats(value) {
+      return `${value} Ks`;
+    },
+  },
+});
